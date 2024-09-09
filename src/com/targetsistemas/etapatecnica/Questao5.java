5a9abc66aeecfd2475f4996dbb9c11104f05cf02
Marco Antonio <marcosdesouza772@gmail.com>
2024-09-09 14:59:08 -0300

    Questão 5

---
+++ src/com/targetsistemas/etapatecnica/Questao5.java
@@ -0,0 +1,25 @@
+package com.targetsistemas.etapatecnica;
+
+public class Questao5 {
+	/*
+	 * 5) Você está em uma sala com três interruptores, cada um conectado a uma
+	 *  lâmpada em salas diferentes. Você não pode ver as lâmpadas da sala em que está, 
+	 *  mas pode ligar e desligar os interruptores quantas vezes quiser. Seu objetivo é 
+	 *  descobrir qual interruptor controla qual lâmpada. Como você faria para descobrir, 
+	 *  usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla
+	 *  cada lâmpada?  
+
+	 * 
+	 * /
+	 */
+	/*
+	 * Resposta
+	 * 5.
+		Ligue um dos interruptores e espere um pouco. 
+		Desligue e ligue um segundo interruptor. Vá até a sala.
+		A lâmpada desligada e quente corresponde ao primeiro interruptor,
+		a lâmpada acesa ao segundo e a lâmpada apagada e fria ao terceiro.
+	 * 
+	 * 
+	 */
+}
